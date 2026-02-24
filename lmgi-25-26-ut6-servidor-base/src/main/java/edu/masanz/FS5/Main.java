1bc7719eaa89f8ae54afa4efa800d2937b728bd7
nmanterbul <nmanterbul@educacion.navarra.es>
2026-02-24 12:43:06 +0100

    Proyecto base

---
+++ lmgi-25-26-ut6-servidor-base/src/main/java/edu/masanz/FS5/Main.java
@@ -0,0 +1,44 @@
+package edu.masanz.FS5;
+
+
+import edu.masanz.FS5.database.ConnectionManager;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.rendering.template.JavalinFreemarker;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        logger.info("ARRANCANDO APLICACION");
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("public_css");
+            config.fileRenderer(new JavalinFreemarker());
+        }).start(8080);
+
+
+
+        // PRINCIPAL
+        app.get("/", Main::ejemplo);
+
+
+
+    }
+
+    private static void ejemplo(@NotNull Context context) {
+
+        Map<String,Object> model = new HashMap<>();
+        context.render("/templates/index.ftl",model);
+    }
+
+}
---
+++ lmgi-25-26-ut6-servidor-base/target/classes/templates/index.ftl
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Titulo</title>
+    <link rel="stylesheet" type="text/css" href="/css/style.css"/>
+</head>
+<body>
+    <div class="container">
+        <h1>Titulo</h1>
+        <p class="center">Acceso a la aplicación</p>
+
+    </div>
+</body>
+</html>
